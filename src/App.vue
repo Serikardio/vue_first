68b57e1b9f9f698131548027284e2d3275b06c8d
Ser <misisipi201@mail.ru>
2024-05-06 14:15:44 +0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
   <div id="app">
     <HelloWorld />
     <DinamicComp />
+    <SiteTest />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import DinamicComp from './components/DinamicComp.vue';
+import DinamicComp from './components/DinamicComp.vue'
+import SiteTest from './components/SiteTest.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,DinamicComp,
+    HelloWorld,DinamicComp,SiteTest,
   }
 }
 </script>
---
+++ src/components/SiteTest.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="flex">
+        <div v-for="number in more" :key="number">{{ number }}
+    </div>
+    <div class="mx-4">{{ umnozhit }}</div>
+    <div v-for="number in numbersHight" :key="number">{{ number }}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      numbers: [1,2,3,4,5,6,7,8,9,10],
+      numbersHight: []
+    };
+  },
+  mounted(){
+    this.numbersHight();
+  },
+  computed: {
+    more() {
+      return this.numbers.filter(item => item > 5);
+    },
+    umnozhit() {
+      return this.numbers.map(item => item * 3);
+    },
+    summ() {
+      return this.numbers.reduce((acc, item ) => acc + item , 0);
+    },
+
+  },
+// methods: {
+//     more(){
+//         for(let i=0; this.numbers.lenght; i++){
+//             if (this.numbers[i] > 5){
+//                 this.numbersHight.push(this.numbers[i]);
+//             }
+//         }
+//     }
+//   }
+// };
+</script>
