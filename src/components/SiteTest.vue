849b99459de752a4e606064ecf9a8e33c5be39c9
Ser <misisipi201@mail.ru>
2024-05-06 15:30:37 +0300

    updated

--- src/components/SiteTest.vue
+++ src/components/SiteTest.vue
@@ -1,41 +1,64 @@
 <template>
-    <div>
+    <div class="flex flex-col">
+
         <div v-for="number in more" :key="number">{{ number }}</div>
         <div class="mx-4">{{ umnozhit }}</div>
-        <div>{{ summ }}</div>
-        <div v-for="number in moreThan5" :key="number">{{ number }}</div>
+        <div class="">{{ summ }}</div>
+
+
+        <div class="" v-for="(number) in filtredNums" :key="number">{{ number }}</div>
+        <div>{{ more }}</div>
     </div>
 </template>
+
 <script>
+
 export default {
   data() {
     return {
       numbers: [1,2,3,4,5,6,7,8,9,10],
-      numbersHight: []
+      filtredNums: [],
+      multi: 3
     };
   },
-  mounted(){
-    this.numbersHight();
-  },
-//   computed: {
-//     more() {
-//       return this.numbers.filter(item => item > 5);
-//     },
-//     umnozhit() {
-//       return this.numbers.map(item => item * 3);
-//     },
-//     summ() {
-//       return this.numbers.reduce((acc, item ) => acc + item , 0);
-//     },
-// }
+  computed: {
+    more() {
+
+        const res = [];
+
+        for (let i = 0; i < this.numbers.length; i++){
+            if (this.numbers[i] > 5) {res.push(this.numbers[i]);}
+        }
+
+        return res;
+
+    //   return this.numbers.filter(item => item > 5);
+    },
+    umnozhit() {
+        const res = [];
+        for (let i = 0; i < this.numbers.length; i++){
+            res.push(this.numbers[i] * this.multi);
+        }
+        return res;
+        },
+
+    //   return this.numbers.map(item => item * 3);
+    summ() {
+      return this.numbers.reduce((acc, item ) => acc + item , 0);
+    },
+},
+
+created(){
+    // this.Num()
+},
+
 methods: {
-    moreThan5(){
-        for(let i = 0; i < this.numbers.lenght; i++){
-            if (this.numbers[i] > 5){
-                this.numbersHight.push(this.numbers[i]);
-            }
+    Num(){
+        for (let i = 0; i < this.numbers.length; i++){
+            if (this.numbers[i] > 5) {this.filtredNums.push(this.numbers[i]);}
         }
     }
   }
+
 };
 </script>
