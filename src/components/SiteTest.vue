6d1ae6c10fe9a0027b96709da4d3b3f1cf6543e2
Ser <misisipi201@mail.ru>
2024-05-06 14:26:50 +0300

    apdate

--- src/components/SiteTest.vue
+++ src/components/SiteTest.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="flex">
-        <div v-for="number in more" :key="number">{{ number }}
+    <div>
+        <div v-for="number in more" :key="number">{{ number }}</div>
+        <div class="mx-4">{{ umnozhit }}</div>
+        <div>{{ summ }}</div>
+        <div v-for="number in moreThan5" :key="number">{{ number }}</div>
     </div>
-    <div class="mx-4">{{ umnozhit }}</div>
-    <div v-for="number in numbersHight" :key="number">{{ number }}
-  </div>
 </template>
 <script>
 export default {
@@ -17,26 +17,25 @@ export default {
   mounted(){
     this.numbersHight();
   },
-  computed: {
-    more() {
-      return this.numbers.filter(item => item > 5);
-    },
-    umnozhit() {
-      return this.numbers.map(item => item * 3);
-    },
-    summ() {
-      return this.numbers.reduce((acc, item ) => acc + item , 0);
-    },
-
-  },
-// methods: {
-//     more(){
-//         for(let i=0; this.numbers.lenght; i++){
-//             if (this.numbers[i] > 5){
-//                 this.numbersHight.push(this.numbers[i]);
-//             }
-//         }
-//     }
-//   }
-// };
+//   computed: {
+//     more() {
+//       return this.numbers.filter(item => item > 5);
+//     },
+//     umnozhit() {
+//       return this.numbers.map(item => item * 3);
+//     },
+//     summ() {
+//       return this.numbers.reduce((acc, item ) => acc + item , 0);
+//     },
+// }
+methods: {
+    moreThan5(){
+        for(let i = 0; i < this.numbers.lenght; i++){
+            if (this.numbers[i] > 5){
+                this.numbersHight.push(this.numbers[i]);
+            }
+        }
+    }
+  }
+};
 </script>
