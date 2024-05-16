5942638a88cda0c741e01769c1013fb8bd5a29db
Ser <misisipi201@mail.ru>
2024-05-16 14:56:14 +0300

    new

--- src/components/VueCopy.vue
+++ src/components/VueCopy.vue
@@ -1,95 +1,79 @@
 <template>
   <div>
-    <p class="text-2xl" >Vue test</p>
-    <div>{{ id_num}}</div>
+    <p class="text-2xl">Vue test</p>
+    <div v-if="groupedTags">
+      <div v-for="(tags, section) in groupedTags" :key="section" class="section">
+        <h2>Section: {{ section }}</h2>
+        <ul>
+          <li v-for="tag in tags" :key="tag.id">
+            Name: {{ tag.name }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        tags: [],
-        id_num: 0,
+export default {
+  data() {
+    return {
+      tags: [],
+      id_num: 0,
+    };
+  },
+
+  computed: {
+    groupedTags() {
+      return this.groupBySection(this.tags);
+    },
+  },
+
+  mounted() {
+    this.createTag({name: "tag", section: "sales"});
+    this.createTag({name: "tag", section: "task"});
+    this.createTag({name: "tag", section: "sales"});
+    this.createTag({name: "tag", section: "task"});
+
+    console.log(this.groupedTags);
+
+    // Теперь domTags не требуется, так как отображение происходит через шаблон
+    this.updateTag(3, "NEW");
+    this.removeTag(2);
+
+    console.log(this.tags);
+  },
+
+  methods: {
+    createTag({section, name}) {
+      let tag = {
+        id: ++this.id_num,
+        name: name + this.id_num,
+        section,
       };
+      this.tags.push(tag);
     },
-
-    computed: {
-      groupedTags() {
-        return this.groupBySection(this.tags);
-      },
+    removeTag(id) {
+      let index = this.tags.findIndex(tag => tag.id === id);
+      if (index !== -1) {
+        this.tags.splice(index, 1);
+      }
     },
-
-    mounted() {
-      this.createTag({name: "tag", section: "sales"});
-      this.createTag({name: "tag", section: "task"});
-      this.createTag({name: "tag", section: "sales"});
-      this.createTag({name: "tag", section: "task"});
-
-      console.log(this.groupedTags);
-
-      this.domTags(this.groupedTags);
-
-      this.updateTag(3, "NEW");
-      this.removeTag(2);
-
-      console.log(this.tags);
+    updateTag(id, newName) {
+      let tag = this.tags.find(tag => tag.id === id);
+      if (tag) {
+        tag.name = newName;
+      }
     },
-
-    methods: {
-      createTag({section, name}) {
-        let tag = {
-          id: ++this.id_num,
-          name: name + this.id_num,
-          section,
-        };
-        this.tags.push(tag);
-      },
-      removeTag(id) {
-        let index = this.tags.findIndex(tag => tag.id === id);
-        if (index !== -1) {
-          this.tags.splice(index, 1);
+    groupBySection(tags) {
+      return tags.reduce((res, tag) => {
+        if (!res[tag.section]) {
+          res[tag.section] = [];
         }
-      },
-      updateTag(id, newName) {
-        let tag = this.tags.find(tag => tag.id === id);
-        if (tag) {
-          tag.name = newName;
-        }
-      },
-      groupBySection(tags) {
-        return tags.reduce((res, tag) => {
-          if (!res[tag.section]) {
-            res[tag.section] = [];
-          }
-          res[tag.section].push({name: tag.name, id: tag.id});
-          return res;
-        }, {});
-      },
-      domTags(res) {
-        const container = document.createElement('div');
-        container.id = 'tagContainer';
-        document.body.appendChild(container);
-
-        for (const section in res) {
-          const sectionDiv = document.createElement('div');
-          sectionDiv.className = 'section';
-
-          const sectionHeader = document.createElement('h2');
-          sectionHeader.textContent = "Section: " + section;
-          sectionDiv.appendChild(sectionHeader);
-
-          const tagsList = document.createElement('ul');
-          res[section].forEach(tag => {
-            const tagItem = document.createElement('li');
-            tagItem.textContent = "Name: " + tag.name;
-            tagsList.appendChild(tagItem);
-          });
-
-          sectionDiv.appendChild(tagsList);
-          container.appendChild(sectionDiv);
-        }
-      },
+        res[tag.section].push({name: tag.name, id: tag.id});
+        return res;
+      }, {});
     },
-  };
+  },
+};
 </script>
