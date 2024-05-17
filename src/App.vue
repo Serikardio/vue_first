3fa382c1757c8ac577bfc65b414987a41473feb6
Ser <misisipi201@mail.ru>
2024-05-17 15:43:34 +0300

    new

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld />
-    <DinamicComp />
-    <SiteTest />
     <VueCopy />
 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import DinamicComp from './components/DinamicComp.vue'
-import SiteTest from './components/SiteTest.vue'
 import VueCopy from "@/components/VueCopy.vue";
 
 export default {
-  name: 'App',
   components: {
     VueCopy,
-    HelloWorld,DinamicComp,SiteTest,
   }
 }
 </script>
--- src/components/VueCopy.vue
+++ src/components/VueCopy.vue
@@ -1,34 +1,56 @@
 <template>
   <div class="ml-2">
-    <p class="text-2xl">Vue test</p>
+    <p class="text-3xl pb-4">Vue test</p>
+
     <div>
+      <p class="text-2xl font-medium pb-2">Search</p>
+
+      <div class="flex items-center ">
+        <input v-model="srch" class="border rounded flex items-center px-2" placeholder="Name" >
+        <button class="flex items-center justify-center mx-1 w-20 h-7 bg-blue-500 text-white rounded -mb-0.5 hover:bg-blue-700">Search</button>
+      </div>
+
+    </div>
+
+    <div>
+
       <p class="font-medium text-2xl my-1.5" >Create a Tag</p>
+
       <input v-model="new_name" class="border rounded flex items-center px-2 mb-2" placeholder="Name" >
       <input v-model="new_section" class="border rounded flex items-center px-2 mb-2 " placeholder="Section" >
+
       <div class="flex">
           <button v-on:click="addTag" class="flex items-center justify-center mx-0.5 w-20 h-7 bg-blue-500 text-white rounded -mb-0.5 hover:bg-blue-700" >Create</button>
           <button v-on:click="DeleteTag" class="flex items-center justify-center mx-0.5 w-20 h-7 bg-blue-500 text-white rounded -mb-0.5 hover:bg-blue-700" >Delete</button>
       </div>
+
     </div>
-    <!--Добавить проверку на одинаковые слова в секциях-->
+
     <div v-if="groupedTags">
       <div v-for="(tags, section) in groupedTags" v-bind:key="section" class="section">
+
         <h2 class="font-medium my-1 ">Section: {{ section }}</h2>
         <ul>
-         <li class="font-normal ml-4" v-for="tag in tags" :key="tag.id">
-            <div class="flex items-center">
+
+          <li class="font-normal ml-4" v-for="tag in tags" :key="tag.id">
+
+           <div class="flex items-center">
               Name: {{ tag.name }}
               <button @click="removeTag(tag.id)" class="flex items-center justify-center border rounded p-0.5 w-5 h-5 ml-1">X</button>
               <button @click="Selected(tag.id)" class="flex items-center justify-center border rounded p-0.5 w-5 h-5 ml-1">Y</button>
             </div>
-            <div v-if="editingTagId === tag.id" class="flex items-center">
+
+            <div v-if="selectedID === tag.id" class="flex items-center">
               <input v-model="name" class="border rounded flex items-center px-2 mb-2" placeholder="Name">
-              <button @click="updateTag(tag.id)">Обновить</button>
+              <button @click="updateTag(tag.id)" >Обновить</button>
             </div>
+
           </li>
+
         </ul>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -41,7 +63,8 @@ export default {
       name:"",
       new_name: "",
       new_section: "",
-      editingTagId: null,
+      selectedID: null,
+      srch: "",
     };
   },
 
@@ -68,11 +91,10 @@ export default {
   },
 
   methods: {
-    // DeleteTag() {
-    //     this.tag in tags{
-    //      НУЖЕН метод файнд и найти по имени айди тега и run функцию removeTag(id)
-    //   }
-    // },
+    SearchTeg(){
+
+    },
+
     DeleteTag() {
       let section = this.new_section;
       let name = this.new_name;
@@ -81,6 +103,7 @@ export default {
         this.removeTag(tag.id);
       }
     },
+
     addTag() {
       const newName = this.new_name.trim();
       const newSection = this.new_section.trim();
@@ -95,6 +118,7 @@ export default {
         }
       }
     },
+
     createTag({section, name}) {
       let tag = {
         id: ++this.id_num,
@@ -104,6 +128,7 @@ export default {
       this.tags.push(tag);
       this.saveTags();
     },
+
     removeTag(id) {
       let index = this.tags.findIndex(tag => tag.id === id);
       if (index !== -1) {
@@ -111,6 +136,7 @@ export default {
         this.saveTags();
       }
     },
+
     updateTag(id) {
       let tag = this.tags.find(tag => tag.id === id);
       if (tag) {
@@ -119,6 +145,7 @@ export default {
         this.name = '';
       }
     },
+
     groupBySection(tags) {
       return tags.reduce((res, tag) => {
         if (!res[tag.section]) {
@@ -128,9 +155,11 @@ export default {
         return res;
       }, {});
     },
+
     saveTags() {
       localStorage.setItem('tags', JSON.stringify(this.tags));
     },
+
     loadTags() {
       let storedTags = localStorage.getItem('tags');
       if (storedTags) {
@@ -138,14 +167,17 @@ export default {
         this.id_num = this.tags.length ? this.tags[this.tags.length - 1].id : 0;
       }
     },
+
     Selected(tagId) {
-      if (this.editingTagId === tagId) {
-        this.editingTagId = null;
+      if (this.selectedID === tagId) {
+        this.selectedID = null;
       } else {
-        this.editingTagId = tagId;
+        this.selectedID = tagId;
       }
       this.saveTags();
     },
+
+
   },
 };
 </script>
